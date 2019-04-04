1100ae38fba64560b247b4c68945a27963c380f3
leetaylor7 <ltaylor@goodmanfinancial.com>
2019-04-04 18:52:40 -0500

    reformatted the folder layout'

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ def updateAPI():
     API_get.updateDB()
     pass
 
-# hacky way to possibly get the JSON from API_get
-# this route is more related to what Yang's doing but I figured I'd put this here
-# for test purposes (note that it may not actually work)
-# be sure to delete this route (or modify it) to not use updateDB, since this isn't really what the function's for
+# # hacky way to possibly get the JSON from API_get
+# # this route is more related to what Yang's doing but I figured I'd put this here
+# # for test purposes (note that it may not actually work)
+# # be sure to delete this route (or modify it) to not use updateDB, since this isn't really what the function's for
 @app.route('/api/get')
 def getAPI():
     return jsonify(API_get.updateDB())
