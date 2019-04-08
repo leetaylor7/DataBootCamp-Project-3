67050ee9dec551a21e08d675a6a3c21beff6087a
leetaylor7 <ltaylor@goodmanfinancial.com>
2019-04-07 21:18:30 -0500

    Was able to get a basic plotly chart going

--- static/js/plotlylogic.js
+++ static/js/plotlylogic.js
@@ -1,29 +1,42 @@
-function buildCharts(newYear) {
 
+function buildCharts(Y) {
+
+    var xData = [];
+    var yData = [];
 //   // // @TODO: Use `d3.json` to fetch the sample data for the plots
-//   //  var url = `/api/get`
-//    d3.json(url).then(function(response) {
-//      console.log(Object.entries(response[selection]))
+   var url = `/api/get`
+   d3.json(url).then(function(response) {
+     console.log(Y)
+     var yearInfo = response[1997]
+     var dataKeys = Object.keys(yearInfo)
+     var dataValues = Object.values(yearInfo)
+    //  console.log(dataNames)
+     for (var j = 0; j < dataKeys.length; j++) {
+      //  console.log(dataKeys[j])
+      yData
+       .push(dataValues[j][Y])
+      xData
+       .push(dataKeys[j])
+     }
 
-//   // @TODO: Build a Bubble Chart using the sample data   
-//   trace1 = {
-//    type: "scatter",
-//    x: response.otu_ids,
-//    y: response.sample_values,
-//    mode: "markers",
-//    marker: {
-//      size: response.sample_values,
-//      color: response.otu_ids,
-//    }
-//  };
+     console.log(xData)
+     console.log(yData)
 
-//    var data = [trace1];
+  // @TODO: Build a Bubble Chart using the sample data   
+  trace1 = {
+   type: "bar",
+   x: xData,
+   y: yData,
+   mode: "markers",
+ };
 
-//    var layout = {
-//      title: "Bubble Chart"
-//    };
+   var data = [trace1];
 
-//    Plotly.newPlot("plotly", data, layout);
+   var layout = {
+     title: "Information Breakdown By State"
+   };
+
+   Plotly.newPlot("plotly", data, layout);
 
 //   // @TODO: Build a Pie Chart
 //   // HINT: You will need to use slice() to grab the top 10 sample_values,
@@ -42,56 +55,54 @@ function buildCharts(newYear) {
 //   }
 
 //   Plotly.newPlot("pie", data2, layout2)
-// });
-// }
+});
+}
+
+ // Fetch new data each time a new item is selected
+
 
 function init() {
- // Grab a reference to the dropdown select element
- var selector = d3.select("#selYear");
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selYear");
+ 
+  // Use the list of sample names to populate the select options
+  d3.json("/api/get").then((response) => {
+    var selection = Object.keys(response)
+       selection.forEach((Y) => {
+         selector
+           .append("option")
+           .text(Y)
+           .property("value", Y)
+   //  buildCharts(response);
+   //  buildMetadata(firstSample);
+   });
+   // Use the first sample from the list to build the inSitial plots
+ 
+   var dataSelector = d3.select('#selData')
+   const year = Object.entries(response[1997]);
+     var key = Object.values(year[0])
+     var dataNames = Object.keys(key[1])
+       dataNames.forEach((DN) => {
+         dataSelector
+           .append("option")
+           .text(DN)
+           .property("value", DN)
+       });
+ 
+ 
+     // console.log(firstYear[0][1])
+     // firstYear.forEach((State) => {
+       // console.log(State)
+     // });
+ }); 
+ }
+ 
+ function optionChanged(newData) {
+  var changedData = newData
+  //  console.log(changedData)
+   buildCharts(changedData);
+ }
 
- // Use the list of sample names to populate the select options
- d3.json("/api/get").then((response) => {
-   var selection = Object.keys(response)
-      selection.forEach((Y) => {
-        selector
-          .append("option")
-          .text(Y)
-          .property("value", Y)
-  //  buildCharts(response);
-  //  buildMetadata(firstSample);
-  });
-  // Use the first sample from the list to build the inSitial plots
-
-  var dataSelector = d3.select('#selData')
-  const year = Object.entries(response[1997]);
-    var key = Object.values(year[0])
-    var dataNames = Object.keys(key[1])
-      dataNames.forEach((DN) => {
-        dataSelector
-          .append("option")
-          .text(DN)
-          .property("value", DN)
-      });
-
-
-    // console.log(firstYear[0][1])
-    // firstYear.forEach((State) => {
-      // console.log(State)
-    // });
-}); 
-}
-
-function optionChanged(newYear) {
- // Fetch new data each time a new item is selected
-  buildCharts(newYear);
-}
-
-function dataChanged(newData) {
- // Fetch new data each time a new item is selected
-  buildCharts(newData);
-}
-  
-// // Initialize the dashboard
 init();
 
 // d3.json("/api/get").then((response) => {
