c423a84345973c75707abb93506a4a0729ecd8bf
ejdubec <erikdubec@gmail.com>
2019-04-09 21:08:03 -0500

    updated mapChart.js

--- static/js/mapChart.js
+++ static/js/mapChart.js
@@ -1,15 +1,15 @@
 // Creating map object
 // center on US
-let map = L.map("map", {
+let map = L.map("chart", {
     center: [39.8283, -98.5795],
     zoom: 4
-  });
+});
 
 // adding tile layer
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={key}", {
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={key}", {
     id: "mapbox.streets",
     key: API_KEY
-  }).addTo(map);
+}).addTo(map);
 
 // placeholder for coloring function
 //function chooseColor(state) {
@@ -30,6 +30,9 @@ d3.json('stateboarder.json', function(data) {
               color: "#ffffff",
               weight: 3.5
             };
+        },
+        onEachFeature: function(feature, layer) {
+            getPopup(feature, layer);
         }
     }).addTo(map);
 });
