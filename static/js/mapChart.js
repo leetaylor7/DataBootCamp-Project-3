f3052beead6081dcc486b5f13e57acf2d2ed0f35
ejdubec <erikdubec@gmail.com>
2019-04-09 20:59:26 -0500

    updated mapChart.js

--- static/js/mapChart.js
+++ static/js/mapChart.js
@@ -5,7 +5,31 @@ let map = L.map("map", {
     zoom: 4
   });
 
+// adding tile layer
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={key}", {
     id: "mapbox.streets",
     key: API_KEY
   }).addTo(map);
+
+// placeholder for coloring function
+//function chooseColor(state) {
+//  CODE
+//}
+
+// create popup function
+let getPopup = function(feature, layer) {
+    let content = `<h1>${feature.properties.NAME}</h1>`;
+    layer.bindPopup(content);
+}
+
+// get geoJSON file (stateboarder.json)
+d3.json('stateboarder.json', function(data) {
+    L.geoJSON(data, {
+        style: function(feature) {
+            return {
+              color: "#ffffff",
+              weight: 3.5
+            };
+        }
+    }).addTo(map);
+});
