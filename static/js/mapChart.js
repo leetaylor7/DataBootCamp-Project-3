b671de8f92ed29028d12a3c2c2b933a1a1b0b4ec
leetaylor7 <ltaylor@goodmanfinancial.com>
2019-04-10 21:26:18 -0500

    lees edits to leaflet

--- static/js/mapChart.js
+++ static/js/mapChart.js
@@ -1,60 +1,13 @@
 // Creating map object
 // center on US
-let map = L.map("chart", {
-    center: [39.8283, -98.5795],
-    zoom: 4
-});
-
-// adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={key}", {
-    id: "mapbox.streets",
-    key: API_KEY
-}).addTo(map);
-
-// placeholder for coloring function
-//function chooseColor(state) {
-//  CODE
-//}
-
-// create popup function
-let getPopup = function(feature, layer) {
-    let content = `<h1>${feature.properties.NAME}</h1>`;
-    layer.bindPopup(content);
-}
-
-
-// holds geoJSON layer
-let geoLay;
-// function to 
-// get geoJSON file (stateboarder.json = stateBorders) and lay out Leaflet map
-function setMap(yearFilter, dataFilter, baseFilter, regionFilter) {
-    updateColor();
-    if (geoLay) {
-        map.removeLayer(geoLay);
-    }
-    geoLay = L.geoJSON(stateBorders, {
-        style: function(feature) {
-            return {
-                color: "#000000",
-                fillColor: getColor(feature),
-                fillOpacity: 0.4,
-                weight: 2.5
-            };
-        },
-        onEachFeature: function(feature, layer) {
-            getPopup(feature, layer);
-        }
-    });
-    geoLay.addTo(map);
-}
-
 function secondChart(yearFilter, dataFilter, baseFilter, regionFilter) {
 
-  xData = [];
+  var xData = [];
   var yData = [];
-  comp = [];
+  var comp = [];
   var baseYData = [];
   var baseComp = [];
+  let colorsObj = {};
 //   // // @TODO: Use `d3.json` to fetch the sample data for the plots
  var url = regionFilter[0]
  d3.json(url).then(function(response) {
@@ -89,26 +42,91 @@ function secondChart(yearFilter, dataFilter, baseFilter, regionFilter) {
 // makes the data comparison Chart
 // ============================================================
 var relativeChange = [];
-var compChange = [];
 for (var j = 0; j < dataKeys.length; j++) {
   relativeChange.push(((yData[j] / baseYData[j])-1));
-  compChange.push(((comp[j] / baseComp[j]) - 1));
  }
- console.log(comp)
- console.log(xData)
- console.log([comp, xData])
- //=============================================================
- //=============================================================
- //=============================================================
+
+
+let map = L.map("chart", {
+    center: [39.8283, -98.5795],
+    zoom: 4
+});
+
+// adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={key}", {
+    id: "mapbox.streets",
+    key: API_KEY
+}).addTo(map);
+
+// placeholder for coloring function
+//function chooseColor(state) {
+//  CODE
+//}
+
+// create popup function
+let getPopup = function(feature, layer) {
+    let content = `<h1>${feature.properties.NAME}</h1>`;
+    layer.bindPopup(content);
+}
+function getColor(feature) {
+  // console.log(colorsObj[0]);
+  return colorsObj[feature.properties.NAME];
+}
+var total = 0;
+for (let i = 0; i < relativeChange.length; i++) {
+  total += relativeChange[i]
+}
+var avg = total / relativeChange.length;
+
+// holds geoJSON layer
+let geoLay;
+// function to 
+// get geoJSON file (stateboarder.json = stateBorders) and lay out Leaflet map
+    //  console.log('running updateC');
+      for (let i = 0; i < relativeChange.length; i++) {
+        let colorI;
+        if (relativeChange[i] < Math.max(relativeChange)) {
+          colorI = '#400000';
+        }else if (relativeChange[i] < avg) {
+          colorI = '#ff0000';
+        }else if (relativeChange[i] < Math.min) {
+          colorI = '#00ff00';
+        }else{
+          colorI = '#006400';
+        }
+        colorsObj[xData[i]] = colorI;
+      }
+    if (geoLay) {
+        map.removeLayer(geoLay);
+    }
+    geoLay = L.geoJSON(stateBorders, {
+        style: function(feature) {
+            return {
+                color: "#000000",
+                fillColor: getColor(feature),
+                fillOpacity: 0.4,
+                weight: 2.5
+            };
+        },
+        onEachFeature: function(feature, layer) {
+            getPopup(feature, layer);
+        }
+    });
+    geoLay.addTo(map);
 });
 });
 }
+
+
+ //=============================================================
+ //=============================================================
+ //=============================================================
+
 // initialization function
 function init() {
     // Grab a reference to the dropdown select element
     var selector = d3.select("#selYear");
     var baseSelector = d3.select("#selBase")
-    console.log(regionFilter)
     // Use the list of sample names to populate the select options
     d3.json(regionFilter).then((response) => {
       var selection = Object.keys(response)
@@ -136,7 +154,6 @@ function init() {
          });
     });
     secondChart(yearFilter, dataFilter, baseFilter, regionFilter);
-    setMap(yearFilter, dataFilter, baseFilter, regionFilter);
 }
 
 // ############################################
@@ -149,7 +166,7 @@ let regionFilter = ['/api/get_state'];
 let compareFilter = ["GDP (millions of dollars)"];
 let comp;
 let xData;
-let colorsObj = {};
+
 
 //  onChange functions
 function optionChanged(newYear) {
@@ -159,7 +176,6 @@ function optionChanged(newYear) {
       yearFilter.shift();
     }
     secondChart(yearFilter, dataFilter, baseFilter, regionFilter);
-    setMap(yearFilter, dataFilter, baseFilter, regionFilter);
 }
 function dataChanged(newData) {
     dataFilter.push(newData)
@@ -168,7 +184,6 @@ function dataChanged(newData) {
       dataFilter.shift();
     }
     secondChart(yearFilter, dataFilter, baseFilter, regionFilter);
-    setMap(yearFilter, dataFilter, baseFilter, regionFilter);
 }
 function baseChanged(newBase) {
     baseFilter.push(newBase)
@@ -177,7 +192,6 @@ function baseChanged(newBase) {
       baseFilter.shift();
     }
     secondChart(yearFilter, dataFilter, baseFilter, regionFilter);
-    setMap(yearFilter, dataFilter, baseFilter, regionFilter);
 }
 function regionChanged(newRegion) {
     regionFilter.push(newRegion)
@@ -186,45 +200,7 @@ function regionChanged(newRegion) {
       regionFilter.shift();
     }
     secondChart(yearFilter, dataFilter, baseFilter, regionFilter);
-    setMap(yearFilter, dataFilter, baseFilter, regionFilter);
 }
 
-// make the getColor function
-// basically this relies on running secondChart first
-// since I believe? that it updates the values we care about
-// I made comp and xData global variables (array type) which should be updated inside secondChart
-
-// basically this will take the feature and check by state name which color it should have
-// there will be a supplemental function below
-function getColor(feature) {
-  console.log(colorsObj);
-  return colorsObj[feature.properties.NAME];
-}
-
-/* #############################################
-################################################
-#####  MAKE SURE TO RUN updateColor()  BEFORE ##
-################################################
-############################################# */
-
-// supplemental function to update the colorsObj
-// stores state names as keys, colors as values
-// relies on recently running secondChart to have up-to-date [comp, xData]   (#, name)
-function updateColor() {
-//  console.log('running updateC');
-  for (let i = 0; i < comp.length; i++) {
-    let colorI;
-    if (comp[i] < -50000) {
-      colorI = '#400000';
-    }else if (comp[i] < 0) {
-      colorI = '#ff0000';
-    }else if (comp[i] < 50000) {
-      colorI = '#00ff00';
-    }else{
-      colorI = '#006400';
-    }
-    colorsObj[xData[i]] = colorI;
-  }
-}
 
 init();
